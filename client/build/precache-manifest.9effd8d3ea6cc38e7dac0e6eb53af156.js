f644c865c711796406fcbe5cb218b5d71fb92612
UraSerdyuk <yuraserdiuk@gmail.com>
2020-12-18 23:43:26 +0200

    configur gitignore

---
+++ client/build/precache-manifest.9effd8d3ea6cc38e7dac0e6eb53af156.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df81a061712c851d1f38cbb22b052c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bbbf7fd428d625c0cfb",
+    "url": "/static/css/main.d6798948.chunk.css"
+  },
+  {
+    "revision": "e882e41f3c8bba1729a8",
+    "url": "/static/js/2.f3e3ea58.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f3e3ea58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bbbf7fd428d625c0cfb",
+    "url": "/static/js/main.bffb0ea0.chunk.js"
+  },
+  {
+    "revision": "9cf0798c76e0680cdecf",
+    "url": "/static/js/runtime-main.e285e6f1.js"
+  },
+  {
+    "revision": "5652fafe34efcb206c3d274c96f7d781",
+    "url": "/static/media/main.5652fafe.jpg"
+  }
+]);
