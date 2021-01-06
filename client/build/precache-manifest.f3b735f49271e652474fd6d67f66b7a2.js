5147bf92aa4e37beb7d63ac2026e70de9ad7ce06
UraSerdyuk <yuraserdiuk@gmail.com>
2021-01-07 01:48:18 +0200

    change icons

---
+++ client/build/precache-manifest.f3b735f49271e652474fd6d67f66b7a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d46abd44848a927db57cf74e4fc7b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0056ee804fa21960869b",
+    "url": "/static/css/main.d6798948.chunk.css"
+  },
+  {
+    "revision": "b6fa91e6912406e60a00",
+    "url": "/static/js/2.db4074fb.chunk.js"
+  },
+  {
+    "revision": "2881c4a89c3c7ad239c40a9c9582b16d",
+    "url": "/static/js/2.db4074fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0056ee804fa21960869b",
+    "url": "/static/js/main.df092deb.chunk.js"
+  },
+  {
+    "revision": "9cf0798c76e0680cdecf",
+    "url": "/static/js/runtime-main.e285e6f1.js"
+  },
+  {
+    "revision": "5652fafe34efcb206c3d274c96f7d781",
+    "url": "/static/media/main.5652fafe.jpg"
+  }
+]);
