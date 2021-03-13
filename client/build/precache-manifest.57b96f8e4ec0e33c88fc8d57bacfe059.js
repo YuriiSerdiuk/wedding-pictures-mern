8d5c3ea526ef483d9a1371c8a827be0521284eee
yurii-serdiuk-gaming <yurii.serdiuk@authenticgaming.com>
2021-03-13 17:02:30 +0200

    test

--- client/build/precache-manifest.57b96f8e4ec0e33c88fc8d57bacfe059.js
+++ client/build/precache-manifest.57b96f8e4ec0e33c88fc8d57bacfe059.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f8d1d8712205558a5e8c56942ee8cf38",
+    "revision": "e05b6fe52d2cc3008c6bc65b32551610",
     "url": "/index.html"
   },
   {
-    "revision": "7aea6b1ee806377a191a",
+    "revision": "00d8c9c97371626d5e5e",
     "url": "/static/css/2.5ad4c650.chunk.css"
   },
   {
-    "revision": "23fda5d64b3e3a44e231",
+    "revision": "54e3519b0828854a04a3",
     "url": "/static/css/main.d6798948.chunk.css"
   },
   {
-    "revision": "7aea6b1ee806377a191a",
-    "url": "/static/js/2.29704696.chunk.js"
+    "revision": "00d8c9c97371626d5e5e",
+    "url": "/static/js/2.f92456e8.chunk.js"
   },
   {
     "revision": "0624ddd414b2723e1128995f38c19298",
-    "url": "/static/js/2.29704696.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f92456e8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "23fda5d64b3e3a44e231",
-    "url": "/static/js/main.c3202151.chunk.js"
+    "revision": "54e3519b0828854a04a3",
+    "url": "/static/js/main.d3843911.chunk.js"
   },
   {
     "revision": "9cf0798c76e0680cdecf",
