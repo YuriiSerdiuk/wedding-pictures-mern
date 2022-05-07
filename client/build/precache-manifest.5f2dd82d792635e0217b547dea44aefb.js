a4920a1c6b927d77afad7d82c7a73d4342a90677
yurii-serdiuk <78081274+yurii-serdiuk@users.noreply.github.com>
2022-05-08 00:29:54 +0300

    Update .gitignore

---
+++ client/build/precache-manifest.5f2dd82d792635e0217b547dea44aefb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a2c10dd9cb975cc88e45a20c7b7f96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2821d8e61b78ad23beb",
+    "url": "/static/css/2.1697af3e.chunk.css"
+  },
+  {
+    "revision": "ebdbbd23738160d70560",
+    "url": "/static/css/main.f9aad910.chunk.css"
+  },
+  {
+    "revision": "d2821d8e61b78ad23beb",
+    "url": "/static/js/2.2624a2d7.chunk.js"
+  },
+  {
+    "revision": "97f8afd2893963fcb6c55e0dcb86c6e6",
+    "url": "/static/js/2.2624a2d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdbbd23738160d70560",
+    "url": "/static/js/main.4d756a14.chunk.js"
+  },
+  {
+    "revision": "9cf0798c76e0680cdecf",
+    "url": "/static/js/runtime-main.e285e6f1.js"
+  },
+  {
+    "revision": "5652fafe34efcb206c3d274c96f7d781",
+    "url": "/static/media/main.5652fafe.jpg"
+  }
+]);
